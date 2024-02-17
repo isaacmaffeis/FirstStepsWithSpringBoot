a837bc813e3d2a803d2144adcfbdef66d3f5bfc8
isaacmaffeis <isaacmaffeis@gmail.com>
2024-02-17 12:41:15 +0100

    fixed a bug with db configuration

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- PostSQL
---INSERT INTO widgets (id, name, purpose) VALUES
---(1, 'Widget A', 'Used for testing purposes.'),
---(2, 'Widget B', 'Designed for entertainment.'),
---(3, 'Widget C', 'Enhances productivity.'),
---(4, 'Widget D', 'Perfect for outdoor activities.'),
---(5, 'Widget E', 'Improves overall well-being.');
