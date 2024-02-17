301de5ac98a931ff8899a4a7223e375818f8d794
isaacmaffeis <isaacmaffeis@gmail.com>
2024-02-17 21:22:01 +0100

    add entities and repositories

---
+++ src/main/java/com/isaacmaffeis/firststepswithspringboot/app/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package com.isaacmaffeis.firststepswithspringboot.app.repositories;
+
+import com.isaacmaffeis.firststepswithspringboot.app.domain.entities.BookEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<BookEntity, String>,
+        PagingAndSortingRepository<BookEntity, String> {
+}
